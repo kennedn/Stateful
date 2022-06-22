e57ef173aaa8d325df9fc813cea2a60c8e4d49a7
kennedn <kennedn@msn.com>
2022-06-22 12:25:39 +0100

    Added comments and minor feature to clay_function
    - Added back navigation detection + webview closure in clay_function to mitigate bool field bug
    - Comments added throughout clay_function
    - Bespoke clay_function minifier added under tools/poor_mans_minify.py

--- tools/poor_mans_minify.py
+++ tools/poor_mans_minify.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+### Creates a minified version of clay_function.js, stripping comments and newlines. Using commercial
+### minifiers appears to break some functionality within the script (namely field validation).
+### Due to the nature of clay_function, it is converted to a string and sent up to the clay,
+### A byproduct of this is that comments are also sent, making minification desirable.
 import re
 
 def remove_comments(string):
@@ -13,7 +17,7 @@ def remove_comments(string):
             return "" # so we will return empty to remove the comment
         else: # otherwise, we will return the 1st group
             return match.group(1) # captured quoted-string
-    return regex.sub(_replacer, string)
+    return regex.sub(_replacer, string).replace("\n","")
 
 with open("../src/pkjs/data/clay_function.js") as infile, open("../src/pkjs/data/clay_function.min.js", "w") as outfile:
   outfile.write(remove_comments(infile.read()))
